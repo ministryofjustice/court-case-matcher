e1234d6a9420353027692239cc5a3d89f0c4f8e9
chris-moj <christopher.faulkner@digital.justice.gov.uk>
2020-06-01 13:48:31 +0100

    411 - Ensure profile being used when run tests

--- src/test/java/uk/gov/justice/probation/courtcasematcher/health/HealthCheckTest.java
+++ src/test/java/uk/gov/justice/probation/courtcasematcher/health/HealthCheckTest.java
@@ -9,10 +9,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
-
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext
 @RunWith(SpringRunner.class)
 public class HealthCheckTest {
 
