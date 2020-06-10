4a233e8f486650c86239c5f071e3e8be0faa1ea1
chris-moj <christopher.faulkner@digital.justice.gov.uk>
2020-06-10 07:34:34 +0100

    :sparkles: Alter HealthCheckTest

--- src/test/java/uk/gov/justice/probation/courtcasematcher/health/HealthCheckTest.java
+++ src/test/java/uk/gov/justice/probation/courtcasematcher/health/HealthCheckTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+import uk.gov.justice.probation.courtcasematcher.TestConfig;
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -23,7 +24,7 @@ public class HealthCheckTest {
 
     @Before
     public void before() {
-        RestAssured.port = port;
+        TestConfig.configureRestAssuredForIntTest(port);
         RestAssured.basePath = "/actuator";
     }
 
