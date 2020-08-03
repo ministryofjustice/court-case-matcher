a35d5f3233bc3f395acce4c143c9fa3cfeb92a99
chris-moj <christopher.faulkner@digital.justice.gov.uk>
2020-08-03 19:51:34 +0100

    ✨ restore boot 2.3.0

--- src/test/java/uk/gov/justice/probation/courtcasematcher/application/TestMessagingConfig.java
+++ src/test/java/uk/gov/justice/probation/courtcasematcher/application/TestMessagingConfig.java
@@ -1,19 +1,15 @@
 package uk.gov.justice.probation.courtcasematcher.application;
 
 import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Configuration;
 
 import static org.mockito.Mockito.mock;
 
-@TestConfiguration
+@Configuration
 public class TestMessagingConfig {
 
     @Bean
-    @Primary
-    @Qualifier("jmsConnectionFactory")
     public ActiveMQConnectionFactory jmsConnectionFactory() {
         return mock(ActiveMQConnectionFactory.class);
     }
--- src/test/java/uk/gov/justice/probation/courtcasematcher/health/HealthCheckTest.java
+++ src/test/java/uk/gov/justice/probation/courtcasematcher/health/HealthCheckTest.java
@@ -1,5 +1,4 @@
 package uk.gov.justice.probation.courtcasematcher.health;
-
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import io.restassured.RestAssured;
 import org.junit.Before;
@@ -8,11 +7,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import uk.gov.justice.probation.courtcasematcher.TestConfig;
-import uk.gov.justice.probation.courtcasematcher.application.TestMessagingConfig;
 
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 import static io.restassured.RestAssured.given;
@@ -21,7 +18,6 @@ import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @RunWith(SpringRunner.class)
 @ActiveProfiles("test")
-@Import(TestMessagingConfig.class)
 public class HealthCheckTest {
 
     @LocalServerPort
@@ -29,8 +25,8 @@ public class HealthCheckTest {
 
     @Rule
     public WireMockRule wireMockRule = new WireMockRule(wireMockConfig()
-            .port(8090)
-            .usingFilesUnderClasspath("mocks"));
+        .port(8090)
+        .usingFilesUnderClasspath("mocks"));
 
     @Before
     public void before() {
@@ -42,11 +38,11 @@ public class HealthCheckTest {
     public void testUp() {
 
         String response = given()
-                .when()
-                .get("/health")
-                .then()
-                .statusCode(200)
-                .extract().response().asString();
+            .when()
+            .get("/health")
+            .then()
+            .statusCode(200)
+            .extract().response().asString();
 
         assertThatJson(response).node("status").isEqualTo("UP");
         assertThatJson(response).node("components.jms.status").isEqualTo("UP");
@@ -54,6 +50,4 @@ public class HealthCheckTest {
         assertThatJson(response).node("components.courtCaseService.status").isEqualTo("UP");
         assertThatJson(response).node("components.nomisAuth.status").isEqualTo("UP");
     }
-
 }
-
