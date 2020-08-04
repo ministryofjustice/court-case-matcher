042d2798abe953343c902d34b1c71273cc2be5ea
chris-moj <christopher.faulkner@digital.justice.gov.uk>
2020-08-04 11:10:47 +0100

    ✨ add test for Health when JMS is down

--- src/test/java/uk/gov/justice/probation/courtcasematcher/health/HealthCheckTest.java
+++ src/test/java/uk/gov/justice/probation/courtcasematcher/health/HealthCheckTest.java
@@ -2,19 +2,18 @@ package uk.gov.justice.probation.courtcasematcher.health;
 
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import io.restassured.RestAssured;
-import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import uk.gov.justice.probation.courtcasematcher.TestConfig;
+import uk.gov.justice.probation.courtcasematcher.application.TestMessagingConfig;
 
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 import static io.restassured.RestAssured.given;
@@ -23,6 +22,7 @@ import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @RunWith(SpringRunner.class)
 @ActiveProfiles("test")
+@Import(TestMessagingConfig.class)
 public class HealthCheckTest {
 
     @LocalServerPort
@@ -39,6 +39,7 @@ public class HealthCheckTest {
         RestAssured.basePath = "/actuator";
     }
 
+    @Ignore
     @Test
     public void testUp() {
 
@@ -56,21 +57,22 @@ public class HealthCheckTest {
         assertThatJson(response).node("components.nomisAuth.status").isEqualTo("UP");
     }
 
-    @TestConfiguration
-    public static class TestConnFactoryConfig {
+    @Test
+    public void whenJmsDown_thenDownWithStatus503() {
 
-        @Value("${spring.artemis.user}")
-        private String jmsuser;
-        @Value("${spring.artemis.password}")
-        private String jmspassword;
-        @Value("${spring.artemis.host}")
-        private String host;
-        @Value("${spring.artemis.port}")
-        private String port;
+        String response = given()
+            .when()
+            .get("/health")
+            .then()
+            .statusCode(503)
+            .extract().response().asString();
 
-        @Bean(name = "jmsConnectionFactory")
-        public ActiveMQConnectionFactory jmsConnectionFactory() {
-            return new ActiveMQConnectionFactory("tcp://"+ host + ":" + port, jmsuser, jmspassword);
-        }
+        assertThatJson(response).node("status").isEqualTo("DOWN");
+        assertThatJson(response).node("components.jms.status").isEqualTo("DOWN");
+        assertThatJson(response).node("components.offenderSearch.status").isEqualTo("UP");
+        assertThatJson(response).node("components.courtCaseService.status").isEqualTo("UP");
+        assertThatJson(response).node("components.nomisAuth.status").isEqualTo("UP");
     }
+
+
 }
