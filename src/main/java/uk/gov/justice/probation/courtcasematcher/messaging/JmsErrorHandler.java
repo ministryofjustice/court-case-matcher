bbf2fa882d89cd588ae685f489889544cc720501
chris-moj <christopher.faulkner@digital.justice.gov.uk>
2020-08-05 09:14:56 +0100

    ✅ Added test and code to obscure name

--- src/main/java/uk/gov/justice/probation/courtcasematcher/messaging/JmsErrorHandler.java
+++ src/main/java/uk/gov/justice/probation/courtcasematcher/messaging/JmsErrorHandler.java
@@ -1,8 +1,10 @@
 package uk.gov.justice.probation.courtcasematcher.messaging;
 
+import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ErrorHandler;
+import org.springframework.util.StringUtils;
 
 @Slf4j
 @Service
@@ -10,7 +12,12 @@ public class JmsErrorHandler implements ErrorHandler {
 
     @Override
     public void handleError(Throwable throwable) {
-        log.error("Unexpected error processing message", throwable);
+        String message = throwable.getMessage();
+        log.error("Unexpected error processing message", throwable.getCause());
+        Optional.ofNullable(message).ifPresent(msg -> {
+            log.error("Source message {}", message.replaceAll("(?s)<def_name[^>]*>.*?</def_name>",
+                "<def_name>*****</def_name>"));
+        });
     }
 
 }
