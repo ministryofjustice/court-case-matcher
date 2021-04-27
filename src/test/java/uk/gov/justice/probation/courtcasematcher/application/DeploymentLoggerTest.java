4ebc3e9f365b9f9bf634e08ef1cd7a032d730bc4
chris-moj <christopher.faulkner@digital.justice.gov.uk>
2021-04-27 13:43:30 +0100

    ✨ PIC-1325 Add deployment logging of app on startup

---
+++ src/test/java/uk/gov/justice/probation/courtcasematcher/application/DeploymentLoggerTest.java
@@ -0,0 +1,29 @@
+package uk.gov.justice.probation.courtcasematcher.application;
+
+import java.net.UnknownHostException;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.info.BuildProperties;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class DeploymentLoggerTest {
+    @Mock
+    private BuildProperties buildProperties;
+
+    @Test
+    public void shouldLogInfo() throws UnknownHostException {
+        when(buildProperties.getVersion()).thenReturn("expected_version");
+        final var logger = new DeploymentLogger(buildProperties);
+
+        TestAppender.events.clear();
+        logger.onApplicationEvent(null);
+        assertThat(TestAppender.events.size()).isEqualTo(1);
+        assertThat(TestAppender.events.get(0).toString()).startsWith("[INFO] Starting CourtCaseServiceApplication expected_version using Java ");
+    }
+}
