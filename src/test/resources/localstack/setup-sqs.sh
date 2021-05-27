236d947f5ea6f8fb7ca6b18790510ffd2be493a5
chris-moj <christopher.faulkner@digital.justice.gov.uk>
2021-05-27 15:54:06 +0100

    ✨ PIC-1331 Add SNS in CircleCI

--- src/test/resources/localstack/setup-sqs.sh
+++ src/test/resources/localstack/setup-sqs.sh
@@ -15,7 +15,9 @@ aws --endpoint-url http://localhost:4566 sqs create-queue --queue-name crime-por
 aws --endpoint-url=http://localhost:4566 sqs set-queue-attributes --queue-url "http://localhost:4566/queue/crime-portal-gateway-queue" --attributes '{"RedrivePolicy":"{\"maxReceiveCount\":\"1\", \"deadLetterTargetArn\":\"arn:aws:sqs:eu-west-2:000000000000:crime-portal-gateway-dlq\"}"}'
 aws --endpoint-url=http://localhost:4566 sqs set-queue-attributes --queue-url "http://localhost:4566/queue/court-case-matcher-queue" --attributes '{"RedrivePolicy":"{\"maxReceiveCount\":\"1\", \"deadLetterTargetArn\":\"arn:aws:sqs:eu-west-2:000000000000:court-case-matcher-dlq\"}"}'
 
-aws --endpoint-url=http://localhost:4566 sns create-topic --name court-case-events-topic > /dev/null 2>&1
+aws --endpoint-url=http://localhost:4566 sns create-topic --name court-case-events-topic
 aws --endpoint-url=http://localhost:4566 sns subscribe --topic-arn "arn:aws:sns:eu-west-2:000000000000:court-case-events-topic" --protocol "sqs" --notification-endpoint "http://localhost:4566/000000000000/court-case-matcher-queue"
 
 echo "Configured"
+
+exit 0
