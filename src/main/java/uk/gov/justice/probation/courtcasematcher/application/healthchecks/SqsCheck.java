4a2326a695e14d627112d820ba62400141c3d524
John Evans <john.evans@digital.justice.gov.uk>
2021-07-27 10:10:45 +0100

    🔥 PIC-1374: Remove sqs-ccm-messaging profile and make default

--- src/main/java/uk/gov/justice/probation/courtcasematcher/application/healthchecks/SqsCheck.java
+++ src/main/java/uk/gov/justice/probation/courtcasematcher/application/healthchecks/SqsCheck.java
@@ -3,13 +3,11 @@ package uk.gov.justice.probation.courtcasematcher.application.healthchecks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.ReactiveHealthIndicator;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 import uk.gov.justice.probation.courtcasematcher.service.SqsService;
 
 @Component
-@Profile("sqs-ccm-messaging")
 public class SqsCheck implements ReactiveHealthIndicator {
 
     @Autowired
