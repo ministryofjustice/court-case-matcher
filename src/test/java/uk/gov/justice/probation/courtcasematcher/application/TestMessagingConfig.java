c56d4780509609752bae155f6bc1cddaf3e94527
John Evans <john.evans@digital.justice.gov.uk>
2021-10-26 11:45:18 +0100

    🔥 Pic 1692 - Remove pacts for unused endpoints (#315)
    * 💚 PIC-1446: Fix Pact tagging
    
    * 🔥 PIC-1446: Remove unused JMQ dependencies

--- src/test/java/uk/gov/justice/probation/courtcasematcher/application/TestMessagingConfig.java
+++ src/test/java/uk/gov/justice/probation/courtcasematcher/application/TestMessagingConfig.java
@@ -1,27 +1,12 @@
 package uk.gov.justice.probation.courtcasematcher.application;
 
-import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
-import org.springframework.boot.actuate.jms.JmsHealthIndicator;
 import org.springframework.boot.info.BuildProperties;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Bean;
 import uk.gov.justice.probation.courtcasematcher.application.healthchecks.SqsCheck;
-
-import static org.mockito.Mockito.mock;
 
 @TestConfiguration
 public class TestMessagingConfig {
-
-    @Bean
-    public ActiveMQConnectionFactory jmsConnectionFactory() {
-        return mock(ActiveMQConnectionFactory.class);
-    }
-
-    @Bean
-    public JmsHealthIndicator jmsHealthIndicator() {
-        return mock(JmsHealthIndicator.class);
-    }
 
     @MockBean
     public SqsCheck sqsCheck;
