88a0db1c674d41a1632e48d5c17e0277940a8cef
John Evans <john.evans@digital.justice.gov.uk>
2022-07-15 17:21:58 +0100

    🐛 PIC-2232: Fix error deserialising CCSOffender

--- src/main/java/uk/gov/justice/probation/courtcasematcher/restclient/model/courtcaseservice/CCSOffender.java
+++ src/main/java/uk/gov/justice/probation/courtcasematcher/restclient/model/courtcaseservice/CCSOffender.java
@@ -1,10 +1,15 @@
 package uk.gov.justice.probation.courtcasematcher.restclient.model.courtcaseservice;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
 public class CCSOffender {
     private final String pnc;
     private final String cro;
