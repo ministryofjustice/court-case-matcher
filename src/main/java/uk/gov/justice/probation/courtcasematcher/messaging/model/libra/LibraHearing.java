93e551e60ccd9b4ede58c330d1426dafefa97df1
Jaspal Panesar <jaspal.panesar@digital.justice.gov.uk>
2024-10-01 07:38:26 +0100

    PIC-4019: Libra hearings use c_id instead of case_id, so this changes… (#637)
    * PIC-4019: Libra hearings use c_id instead of case_id, so this changes it to propergate the c_id value in its place
    
    * PIC-4019: Fix for integration test
    
    * PIC-4019: Fix for integration test; rename cId property
    
    (cherry picked from commit 55d8db39601642e731b2f370672df73079fbe5c0)

--- src/main/java/uk/gov/justice/probation/courtcasematcher/messaging/model/libra/LibraHearing.java
+++ src/main/java/uk/gov/justice/probation/courtcasematcher/messaging/model/libra/LibraHearing.java
@@ -56,6 +56,8 @@ public class LibraHearing {
     private final String courtRoom;
     private final LocalDateTime sessionStartTime;
 
+    private final String cid;
+
     public Hearing asDomain() {
         return HearingMapper.newFromLibraHearing(this);
     }
