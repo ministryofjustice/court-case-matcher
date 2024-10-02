224a3cd33395450eee7fc25356e85543d60dae6c
Mark Rees <mark.rees@digital.justice.gov.uk>
2024-10-02 13:41:14 +0100

    PIC-4207 reads hearings from CSV file

--- src/main/java/uk/gov/justice/probation/courtcasematcher/controller/Replay404HearingsController.java
+++ src/main/java/uk/gov/justice/probation/courtcasematcher/controller/Replay404HearingsController.java
@@ -1,24 +1,38 @@
 package uk.gov.justice.probation.courtcasematcher.controller;
 
+
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Supplier;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 @RestController
 public class Replay404HearingsController {
 
+    private final String pathToCsv;
+
+    public Replay404HearingsController(@Value("${replay404.path-to-csv}") String pathToCsv){
+        this.pathToCsv = pathToCsv;
+    }
 
     @PostMapping("/replay404Hearings")
     public String replay404Hearings() {
 
-        // Input
-        // CSV Data from AppInsights with the hearings
 
-        // check court-case-service and compare the last updated date with the inputted-hearing
+
         // if inputted-hearing has a last updated date > court-case-service hearing
         // then call the court case matcher hearing processor
 
-
-        // processing will take a long time, so we need to prevent timeouts during long processing
+        // check how long thread timeout is
+        CompletableFuture.supplyAsync(replay404s(pathToCsv));
         // should provide logs to show progression
 
         // Output
@@ -28,8 +42,37 @@ public class Replay404HearingsController {
         // put a few updated hearings (minor updates) in dev/preprod to the S3 bucket
         // call this endpoint to pick up on those unprocessed hearings in the s3 bucket and process them
 
+        // dry run option?
+        System.out.println("Finished processing");
 
         return "OK";
     }
+
+    @NotNull
+    private static Supplier<String> replay404s(String path) {
+        return () -> {
+            try {
+                System.out.println("About to read file");
+                // Input
+                // CSV Data from AppInsights with the hearings
+                for (String hearing : Files.readAllLines(Paths.get(path), UTF_8)) {
+                    String id = hearing.split(",")[0];
+                    String s3Path = hearing.split(",")[1];
+
+                    System.out.println("ID " +  id + " Path = "+ s3Path);
+                    // check court-case-service and compare the last updated date with the inputted-hearing
+
+
+                }
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            // do some processing
+            System.out.println("file has been read");
+            return "Finished!";
+        };
+    }
+
 
 }
--- src/test/java/uk/gov/justice/probation/courtcasematcher/controller/Replay404HearingsControllerIntTest.java
+++ src/test/java/uk/gov/justice/probation/courtcasematcher/controller/Replay404HearingsControllerIntTest.java
@@ -23,13 +23,14 @@ public class Replay404HearingsControllerIntTest {
     protected int port;
 
     @Test
-    void givenThereAreMessagesOnDlq_whenRetryAllDlqInvoked_shouldReplayMessages() {
+    void givenThereAreMessagesOnDlq_whenRetryAllDlqInvoked_shouldReplayMessages() throws InterruptedException {
 
         WebClient webClient = WebClient.builder()
             .build();
         String replay404HearingsUrl = String.format("http://localhost:%d/replay404Hearings", port);
         String OK = webClient.post().uri(URI.create(replay404HearingsUrl))
             .retrieve().bodyToMono(String.class).block();
+        Thread.sleep(2000);
         assertThat(OK.equals("OK")).isTrue();
 
     }
