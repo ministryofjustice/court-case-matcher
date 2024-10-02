8e000ccb655f0ddba5f29e48f6f7d803e8a61e2e
Abdirizak Obsiye <Obsiye@users.noreply.github.com>
2024-10-02 11:48:56 +0100

    Add comments to be removed

--- src/main/java/uk/gov/justice/probation/courtcasematcher/controller/Replay404HearingsController.java
+++ src/main/java/uk/gov/justice/probation/courtcasematcher/controller/Replay404HearingsController.java
@@ -9,6 +9,26 @@ public class Replay404HearingsController {
 
     @PostMapping("/replay404Hearings")
     public String replay404Hearings() {
+
+        // Input
+        // CSV Data from AppInsights with the hearings
+
+        // check court-case-service and compare the last updated date with the inputted-hearing
+        // if inputted-hearing has a last updated date > court-case-service hearing
+        // then call the court case matcher hearing processor
+
+
+        // processing will take a long time, so we need to prevent timeouts during long processing
+        // should provide logs to show progression
+
+        // Output
+        // HTTP status
+
+        // How to test end-2-end
+        // put a few updated hearings (minor updates) in dev/preprod to the S3 bucket
+        // call this endpoint to pick up on those unprocessed hearings in the s3 bucket and process them
+
+
         return "OK";
     }
 
