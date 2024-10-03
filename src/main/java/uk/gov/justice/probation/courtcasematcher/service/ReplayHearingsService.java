8f9c8967926c1be39765154dfba21a67e53c5fa2
Abdirizak Obsiye <Obsiye@users.noreply.github.com>
2024-10-03 14:31:56 +0100

    Add azure custom event logs

--- src/main/java/uk/gov/justice/probation/courtcasematcher/service/ReplayHearingsService.java
+++ src/main/java/uk/gov/justice/probation/courtcasematcher/service/ReplayHearingsService.java
@@ -1,4 +1,4 @@
-package uk.gov.justice.probation.courtcasematcher.controller;
+package uk.gov.justice.probation.courtcasematcher.service;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -6,13 +6,10 @@ import com.microsoft.applicationinsights.TelemetryClient;
 import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
 import uk.gov.justice.probation.courtcasematcher.messaging.HearingProcessor;
 import uk.gov.justice.probation.courtcasematcher.messaging.MessageParser;
 import uk.gov.justice.probation.courtcasematcher.messaging.model.commonplatform.CPHearingEvent;
 import uk.gov.justice.probation.courtcasematcher.restclient.CourtCaseServiceClient;
-import uk.gov.justice.probation.courtcasematcher.service.TelemetryEventType;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -22,14 +19,12 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.CompletableFuture;
 import java.util.function.Supplier;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
-@RestController
 @Slf4j
-public class Replay404HearingsController {
+public class ReplayHearingsService {
 
     private final String pathToCsv;
     private final CourtCaseServiceClient courtCaseServiceClient;
@@ -40,14 +35,12 @@ public class Replay404HearingsController {
     private final boolean dryRunEnabled;
     private final TelemetryClient telemetryClient;
 
-    public Replay404HearingsController(@Value("${replay404.path-to-csv}") String pathToCsv,
-                                       CourtCaseServiceClient courtCaseServiceClient, AmazonS3 s3Client,
-                                       @Value("${crime-portal-gateway-s3-bucket}") String bucketName,
-                                       final MessageParser<CPHearingEvent> commonPlatformParser,
-                                       final HearingProcessor hearingProcessor,
-                                       final TelemetryClient telemetryClient,
-                                       @Value("${replay404.dry-run}") boolean dryRunEnabled
-                                       )
+    public ReplayHearingsService(@Value("${replay404.path-to-csv}") String pathToCsv,
+                                 CourtCaseServiceClient courtCaseServiceClient, AmazonS3 s3Client,
+                                 @Value("${crime-portal-gateway-s3-bucket}") String bucketName,
+                                 final MessageParser<CPHearingEvent> commonPlatformParser,
+                                 final HearingProcessor hearingProcessor,
+                                 @Value("${replay404.dry-run}") boolean dryRunEnabled, TelemetryClient telemetryClient)
     {
         this.pathToCsv = pathToCsv;
         this.courtCaseServiceClient = courtCaseServiceClient;
@@ -59,25 +52,9 @@ public class Replay404HearingsController {
         this.telemetryClient = telemetryClient;
     }
 
-    @PostMapping("/replay404Hearings")
-    public String replay404Hearings() {
-        // put some proper exception handling in place
-
-        // check how long thread timeout is
-        log.info("Starting to replay 404 hearings");
-        CompletableFuture.supplyAsync(replay404s());
-
-        // How to test end-2-end
-        // put a few updated hearings (minor updates) in dev/preprod to the S3 bucket
-        // call this endpoint to pick up on those unprocessed hearings in the s3 bucket and process them
-
-        // dry run option?
-
-        return "OK";
-    }
 
     @NotNull
-    private Supplier<String> replay404s() {
+    public Supplier<String> replay() {
         return () -> {
             try {
                 List<String> hearingsWith404 = Files.readAllLines(Paths.get(pathToCsv), UTF_8);
