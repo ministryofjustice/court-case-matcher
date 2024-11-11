5bf5607843430826f6f52d6c803868b1d2dbc707
markreesmoj <76954782+markreesmoj@users.noreply.github.com>
2024-11-11 11:22:41 +0000

    PIC-4146 convert to hmpps sqs library (#680)
    * PIC-4146 convert to hmpps sqs library
    
    * PIC-4146 run tests with localstack so that applicationContext can be initialised against SQS
    
    * PIC-4146 WIP - just need to get messageId and operationId working correctly
    
    * PIC-4146 operationId will be present because distributed tracing is now enabled in the library. MessageId is now id - need to verify this
    
    * PIC-4146 run pact tests with localstack
    
    * PIC-4146 remove unused file

--- src/test/java/uk/gov/justice/probation/courtcasematcher/application/TestMessagingConfig.java
+++ src/test/java/uk/gov/justice/probation/courtcasematcher/application/TestMessagingConfig.java
@@ -10,8 +10,6 @@ import org.springframework.boot.info.BuildProperties;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
-import uk.gov.justice.probation.courtcasematcher.application.healthchecks.SqsCheck;
-import uk.gov.justice.probation.courtcasematcher.service.SqsAdminService;
 
 @TestConfiguration
 public class TestMessagingConfig {
@@ -33,12 +31,7 @@ public class TestMessagingConfig {
     }
 
     @MockBean
-    public SqsCheck sqsCheck;
-
-    @MockBean
     private BuildProperties buildProperties;
 
-    @MockBean
-    private SqsAdminService sqsAdminService;
 
 }
