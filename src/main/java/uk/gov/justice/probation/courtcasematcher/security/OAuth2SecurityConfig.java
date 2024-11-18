0258fbab11e2c9fdfc6645cd96526c295434e9df
Abdirizak Obsiye <Obsiye@users.noreply.github.com>
2024-11-18 14:56:50 +0000

    PIC-4243 Add info endpoint
    Authorise public access to the info endpoint
    
    Add test
    
    Remove build properties from TestMessagingConfig to pass test
    
    Fix application local to run server locally

--- src/main/java/uk/gov/justice/probation/courtcasematcher/security/OAuth2SecurityConfig.java
+++ src/main/java/uk/gov/justice/probation/courtcasematcher/security/OAuth2SecurityConfig.java
@@ -26,7 +26,7 @@ public class OAuth2SecurityConfig {
             .and().oauth2Client()
             .and()
             .authorizeHttpRequests(authorize -> authorize
-                .requestMatchers( "/health/**","/queue-admin/retry-all-dlqs", "/replay404Hearings").permitAll()
+                .requestMatchers( "/health/**","/queue-admin/retry-all-dlqs", "/replay404Hearings", "/info").permitAll()
                 .anyRequest().hasRole(role))
             .oauth2ResourceServer().jwt().jwtAuthenticationConverter(new AuthAwareTokenConverter());
         http.anonymous();
---
+++ src/test/java/uk/gov/justice/probation/courtcasematcher/info/InfoTest.java
@@ -0,0 +1,42 @@
+package uk.gov.justice.probation.courtcasematcher.info;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import uk.gov.justice.probation.courtcasematcher.TestConfig;
+import uk.gov.justice.probation.courtcasematcher.application.TestMessagingConfig;
+import uk.gov.justice.probation.courtcasematcher.wiremock.WiremockExtension;
+import uk.gov.justice.probation.courtcasematcher.wiremock.WiremockMockServer;
+
+import static io.restassured.RestAssured.given;
+import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+@Import(TestMessagingConfig.class)
+public class InfoTest {
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    public void before() {
+        TestConfig.configureRestAssuredForIntTest(port);
+    }
+
+    @Test
+    public void testUp() {
+        String response = given()
+            .when()
+            .get("/info")
+            .then()
+            .statusCode(200)
+            .extract().response().asString();
+
+        assertThatJson(response).node("git").isPresent();
+        assertThatJson(response).node("build").isPresent();
+    }
+}
