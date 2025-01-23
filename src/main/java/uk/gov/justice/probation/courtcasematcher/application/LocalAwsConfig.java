84dc2c89db0be48b1766ecfaa053927d7a0a0107
Abdirizak Obsiye <Obsiye@users.noreply.github.com>
2025-01-23 14:46:01 +0000

    PIC-4424 Configure S3AsyncClient
    Configure the s3AsyncClient which will be used to get large sns
    messages from S3

---
+++ src/main/java/uk/gov/justice/probation/courtcasematcher/application/LocalAwsConfig.java
@@ -0,0 +1,40 @@
+package uk.gov.justice.probation.courtcasematcher.application;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3AsyncClient;
+import software.amazon.awssdk.services.s3.S3Client;
+
+import java.net.URI;
+
+@Configuration
+@Profile("local")
+public class LocalAwsConfig {
+
+    @Value("${aws.region-name}")
+    private String regionName;
+
+    @Value("${aws.endpoint-url}")
+    private String endpointUrl;
+
+    @Bean
+    public S3Client amazonS3Client(@Value("${aws.region-name}") String regionName) {
+        return S3Client.builder()
+            .region(Region.of(regionName))
+            .build();
+    }
+
+    @Bean
+    public S3AsyncClient amazonS3LocalStackClient() {
+        return S3AsyncClient.builder()
+            .endpointOverride(URI.create(endpointUrl))
+            .forcePathStyle(true)
+            .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create("any", "any")))
+            .region(software.amazon.awssdk.regions.Region.of(regionName)).build();
+    }
+}
--- src/main/java/uk/gov/justice/probation/courtcasematcher/application/S3Config.java
+++ src/main/java/uk/gov/justice/probation/courtcasematcher/application/S3Config.java
@@ -5,16 +5,22 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3AsyncClient;
 import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
-@Profile("!test")
+@Profile("!test & !local")
 public class S3Config {
 
     @Bean
-    public S3Client amazonS3Client(@Value("${aws.region-name}")String regionName) {
+    public S3Client amazonS3Client(@Value("${aws.region-name}") String regionName) {
         return S3Client.builder()
             .region(Region.of(regionName))
             .build();
+    }
+
+    @Bean
+    public S3AsyncClient s3AsyncClient(@Value("${aws.region-name}") String regionName) {
+        return S3AsyncClient.builder().region(software.amazon.awssdk.regions.Region.of(regionName)).build();
     }
 }
